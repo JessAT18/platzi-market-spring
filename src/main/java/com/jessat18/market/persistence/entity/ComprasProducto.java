b15369c4c2445196b747c2ce7c030106cfd3199e
JessAT18 <j-aquino@outlook.com>
2022-06-18 14:12:51 -0400

    Creación ComprasProducto que tiene una llave primaria compuesta.

---
+++ src/main/java/com/jessat18/market/persistence/entity/ComprasProducto.java
@@ -0,0 +1,50 @@
+package com.jessat18.market.persistence.entity;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "compras_productos")
+public class ComprasProducto {
+    @EmbeddedId
+    private ComprasProductoPK id;
+
+    private Long cantidad;
+
+    private Double total;
+
+    private Boolean estado;
+
+    public ComprasProductoPK getId() {
+        return id;
+    }
+
+    public void setId(ComprasProductoPK id) {
+        this.id = id;
+    }
+
+    public Long getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Long cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+}
