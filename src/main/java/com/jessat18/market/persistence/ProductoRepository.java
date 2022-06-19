78850b509cbb16c6810544d47b49a454332e43ae
JessAT18 <j-aquino@outlook.com>
2022-06-18 23:30:45 -0400

    Utilización de query methods en el repositorio.

--- src/main/java/com/jessat18/market/persistence/ProductoRepository.java
+++ src/main/java/com/jessat18/market/persistence/ProductoRepository.java
@@ -4,11 +4,20 @@ import com.jessat18.market.persistence.crud.ProductoCrudRepository;
 import com.jessat18.market.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll() {
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategory(Long idCategoria) {
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(Long cantidad) {
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
     }
 }
--- src/main/java/com/jessat18/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/jessat18/market/persistence/crud/ProductoCrudRepository.java
@@ -3,5 +3,15 @@ package com.jessat18.market.persistence.crud;
 import com.jessat18.market.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto, Long> {
+    //@Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    //List<Producto> getByCategoria(Long idCategoria);
+    //Query method
+    //List<Producto> findByIdCategoria(Long idCategoria);
+    List<Producto> findByIdCategoriaOrderByNombreAsc(Long idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(Long cantidadStock, Boolean estado);
 }
