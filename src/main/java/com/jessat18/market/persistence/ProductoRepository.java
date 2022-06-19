1e26cf34f74faa2690cc6d3359021ceb215c30b4
JessAT18 <j-aquino@outlook.com>
2022-06-18 23:38:32 -0400

    Implementando la anotación Repository

--- src/main/java/com/jessat18/market/persistence/ProductoRepository.java
+++ src/main/java/com/jessat18/market/persistence/ProductoRepository.java
@@ -2,10 +2,14 @@ package com.jessat18.market.persistence;
 
 import com.jessat18.market.persistence.crud.ProductoCrudRepository;
 import com.jessat18.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
+//Indicamos a Spring que esta clase se encarga de interactuar con la base de datos.
+//Component es una generalización de este tipo de anotaciones
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -19,5 +23,17 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(Long cantidad) {
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(Long idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(Long idProducto) {
+        productoCrudRepository.deleteById(idProducto);
     }
 }
