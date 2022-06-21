bc63595ce87ca95db9504e68091d09d8bca65a21
JessAT18 <j-aquino@outlook.com>
2022-06-21 11:54:47 -0400

    Implementando RestController Product Controller (faltan los request http)

---
+++ src/main/java/com/jessat18/market/web/controller/ProductController.java
@@ -0,0 +1,37 @@
+package com.jessat18.market.web.controller;
+
+import com.jessat18.market.domain.Product;
+import com.jessat18.market.domain.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    public List<Product> getAll() {
+        return productService.getAll();
+    }
+
+    public Optional<Product> getProduct(Long productId) {
+        return productService.getProduct(productId);
+    }
+
+    public Optional<List<Product>> getByCategory(Long categoryId) {
+        return productService.getByCategory(categoryId);
+    }
+
+    public Product save(Product product) {
+        return productService.save(product);
+    }
+
+    public Boolean delete(Long productId) {
+        return productService.delete(productId);
+    }
+}
